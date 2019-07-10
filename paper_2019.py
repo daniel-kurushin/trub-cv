b9b91ee1d3b900f0eed5c3e283f7f2029af4f2f9
daniel.kurushin <daniel.kurushin@gmail.com>
2019-07-11 00:20:49 +0500

    images

--- paper_2019.py
+++ paper_2019.py
@@ -9,16 +9,18 @@ Created on Wed Jul 10 14:45:19 2019
 from bs4 import BeautifulSoup as BS
 from PIL import Image
 from urllib.parse import quote
+import re
 
 def _new_tag(soup, name, attrs, content):
     tag = soup.new_tag(name)
-    for k, v in attrs:
-        draw[k] = v
-    if type(content) == str:
-        tag.string = content
-    else:
-        tag.insert(content)
-    
+    for k, v in attrs.items():
+        tag[k] = v
+    if content is not None:
+        if type(content) == str:
+            tag.string = content
+        else:
+            tag.insert(0, content)
+    return tag
 
 def image(soup, name):
     """
@@ -41,40 +43,48 @@ def image(soup, name):
      <text:line-break/>gfhghf
    </text:p>
     """
-    img = Image.open(name)
+    for suff in ['jpeg','png','jpg']:
+        imname = '/home/dan/Yandex.Disk/Src/trub-cv/paper-img/%s.%s' % (name, suff)
+        try:
+            img = Image.open(imname)
+            break
+        except FileNotFoundError:
+            pass
     w, h = img.size
-    mime = 
-    height = 
-    draw_props = {
-        'draw:style-name':"fr1",
-        'draw:name':name,
-        'text:anchor-type':"as-char",
-        'svg:width':"16cm",
-        'svg:height':"%scm" % round(h * 16 / w, 2),
-        'draw:z-index':"0"
-    }
-    p = soup.new_tag('text:p')
-    p['text:style-name'] = 'P3'
-    draw = soup.new_tag('draw:frame')
-    for k, v in draw_props:
-        draw[k] = v
-    image_props = {
-         'xlink:href':quote(name)
-         'xlink:type':"simple"
-         'xlink:show':"embed"
-         'xlink:actuate':"onLoad"
-         'draw:filter-name':"&lt;Все форматы&gt;"
-         'loext:mime-type':"image/%s" % img.format.lower()
-    }
-    
+    img = _new_tag(soup, 
+        'text:p', 
+        {'text:style-name':'P3'},
+        _new_tag(soup,
+            'draw:frame',
+            {
+                'draw:style-name':"fr1",
+                'draw:name':name,
+                'text:anchor-type':"as-char",
+                'svg:width':"16cm",
+                'svg:height':"%scm" % round(h * 16 / w, 2),
+                'draw:z-index':"0"
+            },
+            _new_tag(soup,
+                'draw:image',
+                {
+                    'xlink:href':quote(imname),
+                    'xlink:type':"simple",
+                    'xlink:show':"embed",
+                    'xlink:actuate':"onLoad",
+                    'draw:filter-name':"&lt;Все форматы&gt;",
+                    'loext:mime-type':"image/%s" % img.format.lower()
+                },
+                None
+            )
+        )
+    )
+    img.insert(1, _new_tag(soup,'text:line-break',{},None))
+    img.insert(2,name)
+    return img
 
-doc = BS(open('/tmp/in.fodt').read(), features='xml')
+doc = BS(open('in.fodt').read(), features='xml')
 
 to_insert = doc.find('text:p', place='here')
-
-p = doc.new_tag('text:p')
-p.string = 'sdfdfs'
-to_insert.insert_after(p)
 
 P = []
 L = ''
@@ -85,10 +95,12 @@ for l in [ x.strip('\n') for x in open('paper-2019').readlines() ]:
     else:
         L += ' %s' % l
 
-for p in P:
-    ins = doc.new_tag('text:p')
-    ins['text:style-name'] = 'P2'
-    ins.string = p
+for p in [ x.strip() for x in P]:
+    if p.startswith('Рис.'):
+        name = re.findall(string=p, pattern='Рис. (.*)')[0]
+        ins = image(doc, name)
+    else:
+        ins = _new_tag(doc, 'text:p', {'text:style-name':'P2'}, p)
     to_insert.insert_after(ins)
     to_insert = ins
 
