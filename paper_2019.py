5865e175e381415596906dc7ac6bb5237613d00e
daniel.kurushin <daniel.kurushin@gmail.com>
2019-07-10 19:12:35 +0500

    img

--- paper_2019.py
+++ paper_2019.py
@@ -8,6 +8,40 @@ Created on Wed Jul 10 14:45:19 2019
 
 from bs4 import BeautifulSoup as BS
 
+def image(soup, name):
+    """
+    <text:p text:style-name="P3">
+     <draw:frame
+       draw:style-name="fr1"
+       draw:name="Изображение1"
+       text:anchor-type="as-char"
+       svg:width="16cm"
+       svg:height="7cm"
+       draw:z-index="0">
+       <draw:image xlink:href="Спиралеобразный%20робот.jpeg"
+         xlink:type="simple"
+         xlink:show="embed"
+         xlink:actuate="onLoad"
+         draw:filter-name="&lt;Все форматы&gt;"
+         loext:mime-type="image/jpeg"/>
+     </draw:frame>
+     <text:line-break/>gfhghf
+   </text:p>
+    """
+    height = "%scm" % 7
+    draw_props = {
+        'draw:style-name':"fr1",
+        'draw:name':name,
+        'text:anchor-type':"as-char",
+        'svg:width':"16cm",
+        'svg:height':height,
+        'draw:z-index':"0"
+    }
+    p = soup.new_tag('text:p')
+    p['text:style-name'] = 'P3'
+    draw = soup.new_tag('draw:frame')
+    
+
 doc = BS(open('/tmp/in.fodt').read(), features='xml')
 
 to_insert = doc.find('text:p', place='here')
