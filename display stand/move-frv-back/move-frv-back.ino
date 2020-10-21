38e84dc4a91d3bc9f51e7381822abe3628855e03
Daniel Kurushin <dan973@ya.ru>
2020-10-21 18:52:52 +0500

    speed_switch

--- display stand/move-frv-back/move-frv-back.ino
+++ display stand/move-frv-back/move-frv-back.ino
@@ -3,18 +3,39 @@
 #define STEPS 200
 #define LIMIT_A A1
 #define LIMIT_B A2
+#define FAST 2
+#define SLOW 3
+
 #define ON_LIMIT_A ! digitalRead(LIMIT_A)
 #define ON_LIMIT_B ! digitalRead(LIMIT_B)
 
+#define GO_FAST digitalRead(FAST) == 0 && digitalRead(SLOW) == 1
+#define GO_SLOW digitalRead(FAST) == 1 && digitalRead(SLOW) == 0
+#define GO_NONE digitalRead(FAST) == 1 && digitalRead(SLOW) == 1
+
+#define SP_NONE   0
+#define SP_SLOW   8
+#define SP_FAST 100
+
+enum tstate {ST_IDLE, ST_LIMIT_A, ST_GO_A, ST_LIMIT_B, ST_GO_B, ST_STOP};
 
 Stepper a_stepper(STEPS, 6, 7, 8, 9);
 
+int read_speed()
+{
+  if (GO_FAST) return SP_FAST;
+  if (GO_SLOW) return SP_SLOW;
+  if (GO_NONE) return SP_NONE;  
+}
+
 void setup()
 {
-  a_stepper.setSpeed(8);
+  a_stepper.setSpeed(SP_SLOW);
   Serial.begin(9600);
   pinMode( 5, 1);
   pinMode(10, 1);
+  pinMode(FAST, 0);
+  pinMode(SLOW, 0);
 }
 
 void enable()
@@ -35,13 +56,25 @@ void go(int steps)
   a_stepper.step(steps);
 }
 
-int x = 10;
-String tstates[] = {"ST_IDLE", "ST_LIMIT_A", "ST_GO_A", "ST_LIMIT_B", "ST_GO_B", "ST_ERROR"};
-enum tstate {ST_IDLE, ST_LIMIT_A, ST_GO_A, ST_LIMIT_B, ST_GO_B, ST_ERROR};
 tstate state = ST_IDLE;
+tstate old_state = state;
+int current_speed = 0;
+int old_speed = 0;
 
 void loop()
 {
+  current_speed = read_speed();
+  if (current_speed != old_speed) 
+  {
+    old_speed = current_speed;
+    disable();
+    if (current_speed != SP_NONE) a_stepper.setSpeed(current_speed);
+    else {
+      old_state = state;
+      state = ST_STOP;
+    }
+    delay(100);
+  }
   switch (state)
   {
     case ST_IDLE:
@@ -68,7 +101,13 @@ void loop()
       if (ON_LIMIT_B) state = ST_LIMIT_B;
       else go( 10);
       break;
-    case ST_ERROR:
+    case ST_STOP:
       disable();
+      if (current_speed != SP_NONE) 
+      { 
+        a_stepper.setSpeed(current_speed);
+        state = old_state;
+      }
+      break;
   }
 }
